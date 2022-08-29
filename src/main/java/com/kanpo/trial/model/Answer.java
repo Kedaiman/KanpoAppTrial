45851e3c5af7f5877e0a869f724efe50e0f6d21f
Kedaiman <keita080111@gmail.com>
2022-08-29 14:21:45 +0900

    updating question tree

--- src/main/java/com/kanpo/trial/model/Answer.java
+++ src/main/java/com/kanpo/trial/model/Answer.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -27,7 +27,7 @@ public class Answer {
 	private long id;
 
 	/** 分析結果(漢方リスト) */
-	@OneToMany
+	@ManyToMany
 	@Column
 	private List<Medicine> medicineList;
 
