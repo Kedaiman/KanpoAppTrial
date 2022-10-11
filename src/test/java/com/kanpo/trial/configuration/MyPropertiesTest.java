4fd65c2e722109d13f38514d560f9f3ddf51e164
Kedaiman <keita080111@gmail.com>
2022-10-11 23:18:14 +0900

    use mysql commit

--- src/test/java/com/kanpo/trial/configuration/MyPropertiesTest.java
+++ src/test/java/com/kanpo/trial/configuration/MyPropertiesTest.java
@@ -3,14 +3,14 @@ package com.kanpo.trial.configuration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@TestPropertySource("/application.properties")
+
+@TestPropertySource("/test.properties")
 @SpringBootTest
 public class MyPropertiesTest {
     @Autowired
